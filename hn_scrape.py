d7ab3079ab10b7506539d20c252015e8f8bec014
Anish Thite <anishthite@gmail.com>
2020-10-29 12:14:59 -0400

    add full scraper

--- hn_scrape.py
+++ hn_scrape.py
@@ -3,30 +3,24 @@ from tqdm import tqdm
 import json
 import math
 import requests
-import multiprocessing.pool
-from bs4 import BeautifulSoup
-import html2text
-
-#Needed for proper formatting
-html2text.BODY_WIDTH = 0
-html2text.single_line_break = True
-import lxml.html
-from lxml import etree
-import os.path
-from os import path
-
-
-
 SCORE_THRESH=1
 COMMENT_SCORE_THRESH=-1
 DESC_THRESH=1
 PERCENT_TAKE = 0.5
 HN_URL="https://hacker-news.firebaseio.com/v0/item/"
 RANDOM_SEED=42
-
-
-# Default Python Pool doesn't allow for daemon processes, if we want heirarchical pools
-# we need to subclass Pool/Process since we need daemon processes
+import multiprocessing.pool
+from bs4 import BeautifulSoup
+import html2text
+html2text.BODY_WIDTH = 0
+html2text.single_line_break = True
+import lxml.html
+from lxml import etree
+import os.path
+from os import path
+import time
+import urllib3
+import ast
 class NoDaemonProcess(multiprocessing.Process):
     # make 'daemon' attribute always return False
     def _get_daemon(self):
@@ -35,6 +29,8 @@ class NoDaemonProcess(multiprocessing.Process):
         pass
     daemon = property(_get_daemon, _set_daemon)
 
+# We sub-class multiprocessing.pool.Pool instead of multiprocessing.Pool
+# because the latter is only a wrapper function, not a proper class.
 class MyPool(multiprocessing.pool.Pool):
     Process = NoDaemonProcess
 
@@ -42,20 +38,24 @@ class MyPool(multiprocessing.pool.Pool):
 # Story Functions
 
 def get_check_story(id):
-    """
-    Checks if the id is a good story and starts the comment tree creating process 
-    if need be
-    """
     print(id)
-    # Prevent recomputing 
-    if path.exists(''.join(["data/",str(id)])):
-        print("already did")
+    if path.exists(''.join(["donev2/",str(id)])):
+        print("already did" + ''.join(["donev2/",str(id)]))
         return "already did"
-
-    item = json.loads(requests.get(''.join([HN_URL, str(id),'.json'])).text) 
     
-    # Some items might be dead, if they aren't the attribute usually isn't there
-    # Use try/catch so we don't need to loop through all keys
+    with open(''.join(['donev2/',str(id)]),'w') as donefile:
+        donefile.write('w')
+
+    try:
+       item = json.loads(requests.get(''.join([HN_URL, str(id),'.json'])).text) 
+    except : 
+        time.sleep(2)
+        try:
+            item = json.loads(requests.get(''.join([HN_URL, str(id),'.json'])).text) 
+        except requests.exceptions.SSLError:
+            return "dead"
+
+
     try:
         if item['dead'] == True:
             return "dead"
@@ -64,8 +64,6 @@ def get_check_story(id):
         return "nonexistant"
     except KeyError:
             pass
-
-    # Good Story check
     try:
         if item['type'] != 'story': 
             print('story')
@@ -86,9 +84,6 @@ def get_check_story(id):
 
 
 def parse_story(id,item):
-    """
-        Creates the text representation of the story and starts the comment creation process
-    """
     header = ''.join([item['title'],' - ',item['by'],'\n'])
     try:
         header = ''.join(['\n',header, item['url'], '\n'])
@@ -103,25 +98,22 @@ def parse_story(id,item):
     
     comments = []
    
-    #start comment chain creation in parallel
+    #start comment chains in parallel
+    # map the list of lines into a list of result dicts
     commentpool = MyPool(12)
     comments = commentpool.map(top_comment_parse,item['kids'])
-    commentpool.terminate()
-    
-    # Remove any blank strings in the result list (happens when a subcomment is dead)
+    commentpool.close()
     comments = list(filter(None, comments)) 
    # for comment in item['kids']:
    ##     parsed_comment = top_comment_parse(comment)
    ##     if parsed_comment != '':
    ##         comments.append(parsed_comment)
-
-    #Add comments to story text
     comments = '------\n'.join(comments)
     header = ''.join([header, '\n', comments])
 
 
-    # write to data/{id}
-    with open(''.join(['data/',str(id)]),'w') as outfile:
+
+    with open(''.join(['datav2/',str(id)]),'w') as outfile:
         outfile.write(header)
     return header
 
@@ -129,17 +121,11 @@ def parse_story(id,item):
 # Comment Functions
 
 def check_comment(item):
-    """
-    Decides if a comment is "good"
-    """
-    # For some reason some comments are blank
     try:
         if item['text'] == None:
             return False
     except KeyError:
         return False
-
-    # Same "dead" attribute logic as with stories
     try:
         if item['dead'] == True :
             return False
@@ -149,10 +135,7 @@ def check_comment(item):
 
 
 def choose_next_sub_comment(comment_list):
-    """
-    Chooses the next coment based on a list of candidates,
-    currently uses max_descendant
-    """
+
     
     #take max descendant len
     def get_max_len(item):
@@ -164,55 +147,58 @@ def choose_next_sub_comment(comment_list):
     comment_list.sort(key= lambda x: get_max_len(x), reverse=True)
     return comment_list[0]
 
-    #sort by score and take random one (not used but provided since I already wrote it)
-    #comment_list.sort(key= lambda x: x['score'], reverse=True)
-    #good_inds = math.ceil(PERCENT_TAKE * len(comment_list))
-    #return random.choice(comment_list[:good_inds+1], seed=RANDOM_SEED)
+    #sort by score and take random one
+    comment_list.sort(key= lambda x: x['score'], reverse=True)
+    good_inds = math.ceil(PERCENT_TAKE * len(comment_list))
+    return random.choice(comment_list[:good_inds+1], seed=RANDOM_SEED)
 
 def sub_comment_parse(item, comment_block):
-    """
-    Recursive function to parse comments and their subcomments
-    """
-
-
-# Past attempts at parsing, BS took too long and lxml didn't parse correctly
 #    item['text'] = BeautifulSoup(item['text'], "lxml").get_text(separator="\n")
 #    item['text'] = lxml.html.fromstring(item['text']).text_content()
-
-    #extract html tags
     item['text'] = html2text.html2text(item['text'])
     comment_block = ''.join([comment_block, item['by'],'\n',item['text']])
-    
-    #return if no sub_comments
     try:
         if len(item['kids']) == 0:
             return comment_block
     except KeyError:
         return comment_block 
 
-    #generate kid comment list
     kids = []
     for kid_id in item['kids']:
-        kids.append(json.loads(requests.get(''.join([HN_URL, str(kid_id),'.json'])).text))
+        try:
+            kid = json.loads(requests.get(''.join([HN_URL, str(kid_id),'.json'])).text)
+        except: 
+            time.sleep(2)
+            try:
+                kid = json.loads(requests.get(''.join([HN_URL, str(kid_id),'.json'])).text)
+            except requests.exceptions.SSLError:
+                continue
+        kids.append(kid)
+        with open(''.join(['donev2/',str(kid['id'])]),'w') as donefile:
+            donefile.write('w')
     possible_routes = [kid for kid in kids if check_comment(kid)]
-    
-    #kid comment(s) might be dead so the possible routes list might be empty (return since no good subcomment) 
+    #might be dead
     if len(possible_routes) == 0:
         return comment_block
 
-    #choose next comment
     next_comment = choose_next_sub_comment(possible_routes)
-    
-    #Add to text representation and recurse
     comment_block = ''.join([comment_block, '~~~\n'])
     comment_block = sub_comment_parse(next_comment, comment_block)
     return comment_block
 
 def top_comment_parse(id):
-    """
-    Retrieves the top-levle comment and starts the recursive sub-comment parsing
-    """
-    item = json.loads(requests.get(''.join([HN_URL, str(id),'.json'])).text) 
+    try:
+       item = json.loads(requests.get(''.join([HN_URL, str(id),'.json'])).text) 
+    except: 
+        time.sleep(2)
+        try:
+            item = json.loads(requests.get(''.join([HN_URL, str(id),'.json'])).text) 
+        except requests.exceptions.SSLError:
+            return comment_block
+
+    with open(''.join(['donev2/',str(id)]),'w') as donefile:
+        donefile.write('w')
+    
     comment_block = ''
     if check_comment(item):
         comment_block = sub_comment_parse(item, comment_block)
@@ -225,18 +211,24 @@ def top_comment_parse(id):
 
 
 
-def main(end_id):
+def main(end_id, num_threads):
     # map the list of lines into a list of result dicts
     pool = MyPool(12)
-    resultlist = pool.map(get_check_story,range(1,end_id, 1))
+    
+    with open('story_list','r') as id_file:
+        indlist = id_file.readline().strip("]").strip("[").split(', ')
+    resultlist = pool.map(get_check_story, indlist)
+    pool.close()
     with open('resultlist','w') as result:
-        result.write(resultlist)
+        result.write(str(resultlist))
 
 
 if __name__ == "__main__":
     #get_check_story(200)
-    print(main(10000000))
-    #print(main(245317120))
+    #print(main(2000, 8))
+    #print(main(5000000, 8))
+    #print(main(10000000, 8))
+    print(main(245317120, 8))
 
 
 
